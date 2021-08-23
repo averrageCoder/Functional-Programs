9f630d9bc8edb241b1eb08e93cc76b6f76c120a8
Harsh R Jain <rjainharsh@gmail.com>
2021-08-24 00:02:38 +0530

    ADDED: program on 2D array

---
+++ src/com/bridgelabz/functionalprograms/TwoDimensionalArray.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.functionalprograms;
+
+import java.util.Scanner;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		
+		   int row, col, i, j;
+	       int arr[][] = new int[10][10];
+	       Scanner scan = new Scanner(System.in);
+		   
+	       System.out.print("Enter row (max 10) : ");
+	       row = scan.nextInt();
+	       System.out.print("Enter column (max 10) : ");
+	       col = scan.nextInt();
+		   
+	       System.out.println("Enter " +(row*col)+ " Array Elements : ");
+	       for(i=0; i<row; i++)
+	       {
+	           for(j=0; j<col; j++)
+	           {
+	               arr[i][j] = scan.nextInt();
+	           }
+	       }
+		   
+	       System.out.print("The 2D Array is :\n");
+	       for(i=0; i<row; i++)
+	       {
+	           for(j=0; j<col; j++)
+	           {
+	               System.out.print(arr[i][j]+ "  ");
+	           }
+	           System.out.println();
+	       }
+
+	}
+
+}
